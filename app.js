8fa7f10f2d264904f435c3a436c3b8cd9ef170f4
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2024-11-03 20:18:15 +0400

    One let to const

--- app.js
+++ app.js
@@ -1600,7 +1600,7 @@ class Build{
 		
 		if(builds.length < 6){
 			
-			let create = DOM({tag: 'button', style: ['build-list-item', 'new-build', 'btn-hover', 'color-1'] ,event:['click', () => {
+			const create = DOM({tag: 'button', style: ['build-list-item', 'new-build', 'btn-hover', 'color-1'] ,event:['click', () => {
 				
 				let template = document.createDocumentFragment();
 				
