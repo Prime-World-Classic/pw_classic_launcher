e4ea0a946ca6f16faa38006aac8be7e06019a3fe
x9noz <ilaparusev@gmail.com>
2025-10-06 09:51:37 +0300

    castle-button-play-3 fix#2
    теперь при смене режима не ломается

--- content/app.js
+++ content/app.js
@@ -1514,7 +1514,7 @@ class CastleNAVBAR {
 
 		CastleNAVBAR.mode = modeSelect;
 
-		CastleNAVBAR.body.children[5].style.display = 'block';
+		CastleNAVBAR.body.children[5].style.display = 'flex';
 
 		let background = window.getComputedStyle(CastleNAVBAR.body.children[`1${type}`], null).getPropertyValue('background-image');
 
