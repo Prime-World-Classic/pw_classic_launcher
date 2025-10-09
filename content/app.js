bbeef0678be0fbd334e12bfc6d3f3fa5c7c14ade
Bogdan <81803584+ifstz@users.noreply.github.com>
2025-10-09 18:09:07 +0300

    Добавлен TURN сервер дока

--- content/app.js
+++ content/app.js
@@ -8816,10 +8816,13 @@ class Voice {
 	static peerConnectionConfig = {
 		// проверка stun https://webrtc.github.io/samples/src/content/peerconnection/trickle-ice/
 		iceServers:[
+		{url:'turn:81.88.210.30:3478',credential:'pw',username:'pw'}
+		/*
 		{url:'turn:192.158.29.39:3478?transport=udp',credential:'JZEOEt2V3Qb0y27GRntt2u2PAYA=',username:'28224511:1379330808'},
 		{url:'turn:192.158.29.39:3478?transport=tcp',credential:'JZEOEt2V3Qb0y27GRntt2u2PAYA=',username:'28224511:1379330808'},
 		{url:'turn:turn.bistri.com:80',credential:'homeo',username:'homeo'},
 		{url:'turn:turn.anyfirewall.com:443?transport=tcp',credential:'webrtc',username:'webrtc'}
+		*/
 		]
 		
 	};
