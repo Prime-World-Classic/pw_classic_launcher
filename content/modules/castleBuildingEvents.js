1469f5da66274f677c128b1b71c8829c16fdfa3e
korobkad3v <korobka.d3v@gmail.com>
2025-11-19 19:01:31 +0200

    docs: add JSDocs for CastleBuildingEvents class

--- content/modules/castleBuildingEvents.js
+++ content/modules/castleBuildingEvents.js
@@ -1,9 +1,19 @@
 import { Window } from './window.js';
 
+/**
+ * @class CastleBuildingsEvents
+ * @description Events for castle buildings.
+ */
 export class CastleBuildingsEvents {
+	/**
+	 * Opens the inventory window.
+	 */
 	static library() {
 		Window.show('main', 'inventory');
 	}
+	/**
+	 * Opens the farm window.
+	 */
 	static talent_farm() {
 		Window.show('main', 'farm');
 	}
