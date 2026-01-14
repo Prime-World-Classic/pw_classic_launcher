df346570e85dee6a9ea9ce825ba10d2d542dd147
korobkad3v <korobka.d3v@gmail.com>
2026-01-14 17:24:59 +0200

    WIP: ui keybindings model

--- content/modules/keybindings/keybindings.store.js
+++ content/modules/keybindings/keybindings.store.js
@@ -24,141 +24,156 @@
 export const KeybindStore = {
   keybindsFileModel: {
     sections: [
-      // GLOBAL 
+      // GLOBAL
       {
         name: null,
         binds: [
-          { command: 'console_prevcommand', value: null, negated: false, keys: null },
-          { command: 'console_nextcommand', value: null, negated: false, keys: null },
-          { command: 'console_scrollup', value: null, negated: false, keys: null },
-          { command: 'console_scrolldown', value: null, negated: false, keys: null },
-          { command: 'console_charleft', value: null, negated: false, keys: null },
-          { command: 'console_charright', value: null, negated: false, keys: null },
-          { command: 'console_home', value: null, negated: false, keys: null },
-          { command: 'console_end', value: null, negated: false, keys: null },
-          { command: 'console_runcommand', value: null, negated: false, keys: null },
-          { command: 'console_runcommand', value: null, negated: false, keys: null },
-          { command: 'console_autocomplete', value: null, negated: false, keys: null },
-          { command: 'console_clear', value: null, negated: false, keys: null },
-          { command: 'console_eraselastchar', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_prevcommand', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_nextcommand', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_scrollup', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_scrolldown', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_charleft', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_charright', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_home', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_end', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_runcommand', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_runcommand', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_autocomplete', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_clear', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'console_eraselastchar', value: null, negated: false, keys: null },
+          { type: 'bind_command', command: 'toggle_fps', value: null, negated: false, keys: null },
+          { type: 'bind_command', command: 'screenshot .jpg', value: null, negated: false, keys: null },
+          { type: 'bind_command', command: 'screenshot .png', value: null, negated: false, keys: null },
         ],
       },
 
-      //  adventure_screen (main) 
+      //  adventure_screen (main)
       {
         name: 'adventure_screen',
         binds: [
-          { command: 'cmd_move', value: null, negated: false, keys: null },
-          { command: 'cmd_attack', value: null, negated: false, keys: null },
-          { command: 'cmd_hold', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_move', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_attack', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_hold', value: null, negated: false, keys: null },
 
           // slots 1–24
           ...Array.from({ length: 24 }, (_, i) => ({
+            type: 'bind',
             command: `cmd_action_bar_slot${i + 1}`,
             value: null,
             negated: false,
             keys: null,
           })),
+          { type: 'bind', command: 'self_cast_on', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'self_cast_off', value: null, negated: true, keys: null },
 
-          { command: 'self_cast_on', value: null, negated: false, keys: null },
-          { command: 'self_cast_off', value: null, negated: true, keys: null },
+          { type: 'bind', command: 'cmd_portal', value: null, negated: false, keys: null },
 
-          { command: 'cmd_portal', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cs_toggle_healthbars', value: null, negated: false, keys: null },
 
-          { command: 'cs_toggle_healthbars', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'exit_game', value: null, negated: false, keys: null },
 
-          { command: 'exit_game', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'camera_switch_attach_mode_down', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'camera_switch_attach_mode_up', value: null, negated: true, keys: null },
 
-          { command: 'camera_switch_attach_mode_down', value: null, negated: false, keys: null },
-          { command: 'camera_switch_attach_mode_up', value: null, negated: true, keys: null },
+          { type: 'bind', command: 'show_statistics', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'hide_statistics', value: null, negated: true, keys: null },
 
-          { command: 'show_statistics', value: null, negated: false, keys: null },
-          { command: 'hide_statistics', value: null, negated: true, keys: null },
+          { type: 'bind', command: 'show_charstat', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'show_inventory', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'show_talents', value: null, negated: false, keys: null },
 
-          { command: 'show_charstat', value: null, negated: false, keys: null },
-          { command: 'show_inventory', value: null, negated: false, keys: null },
-          { command: 'show_talents', value: null, negated: false, keys: null },
-
-          { command: 'actionbar_lock_off', value: null, negated: false, keys: null },
-          { command: 'actionbar_lock_on', value: null, negated: true, keys: null },
+          { type: 'bind', command: 'actionbar_lock_off', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'actionbar_lock_on', value: null, negated: true, keys: null },
         ],
       },
 
-      //  GLOBAL 
+      //  GLOBAL
       {
         name: null,
         binds: [
-          { command: 'cmd_cancel', value: null, negated: false, keys: null },
-          { command: 'cs_mouse_wheel_down', value: null, negated: false, keys: null },
-          { command: 'cs_mouse_wheel_up', value: null, negated: true, keys: null },
-          { command: 'exit_bind', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_cancel', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cs_mouse_wheel_down', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cs_mouse_wheel_up', value: null, negated: true, keys: null },
+          { type: 'bind', command: 'exit_bind', value: null, negated: false, keys: null },
         ],
       },
 
-      //  adventure_screen (camera numeric) 
+      //  adventure_screen (camera numeric)
       {
         name: 'adventure_screen',
         binds: [
-          { command: 'camera_zoom', value: '+1.0', negated: false, keys: null },
-          { command: 'camera_zoom', value: '-1.0', negated: false, keys: null },
-          { command: 'camera_forward', value: '+1.0', negated: false, keys: null },
-          { command: 'camera_forward', value: '-1.0', negated: false, keys: null },
-          { command: 'camera_strafe', value: '+1.0', negated: false, keys: null },
-          { command: 'camera_strafe', value: '-1.0', negated: false, keys: null },
-          { command: 'camera_pitch', value: '-1.0', negated: false, keys: null },
-          { command: 'camera_pitch', value: '+1.0', negated: false, keys: null },
-          { command: 'camera_rotate', value: '+1.0', negated: false, keys: null },
-          { command: 'camera_rotate', value: '-1.0', negated: false, keys: null },
-          { command: 'camera_zoom_mouse', value: '-0.003', negated: false, keys: null },
+          { type: 'bind', command: 'camera_zoom', value: '+1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_zoom', value: '-1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_forward', value: '+1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_forward', value: '-1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_strafe', value: '+1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_strafe', value: '-1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_pitch', value: '-1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_pitch', value: '+1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_rotate', value: '+1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_rotate', value: '-1.0', negated: false, keys: null },
+          { type: 'bind', command: 'camera_zoom_mouse', value: '-0.003', negated: false, keys: null },
         ],
       },
 
-      //  GLOBAL (mouse camera) 
+      //  GLOBAL (mouse camera)
       {
         name: null,
         binds: [
-          { command: '+camera_roll', value: null, negated: false, keys: null },
-          { command: '-camera_roll', value: null, negated: false, keys: null },
-          { command: '+camera_upward', value: null, negated: false, keys: null },
-          { command: '-camera_upward', value: null, negated: false, keys: null },
+          { type: 'bind', command: '+camera_roll', value: null, negated: false, keys: null },
+          { type: 'bind', command: '-camera_roll', value: null, negated: false, keys: null },
+          { type: 'bind', command: '+camera_upward', value: null, negated: false, keys: null },
+          { type: 'bind', command: '-camera_upward', value: null, negated: false, keys: null },
 
-          { command: 'camera_rotate_mouse', value: '0.03', negated: false, keys: null },
-          { command: 'camera_pitch_mouse', value: '0.03', negated: false, keys: null },
-          { command: 'camera_strafe_mouse', value: '-0.03', negated: false, keys: null },
-          { command: 'camera_strafe_mouse', value: '0.03', negated: false, keys: null },
-          { command: 'camera_forward_mouse', value: '0.03', negated: false, keys: null },
-          { command: 'camera_forward_mouse', value: '0.03', negated: false, keys: null },
+          { type: 'bind', command: 'camera_rotate_mouse', value: '0.03', negated: false, keys: null },
+          { type: 'bind', command: 'camera_pitch_mouse', value: '0.03', negated: false, keys: null },
+          { type: 'bind', command: 'camera_strafe_mouse', value: '-0.03', negated: false, keys: null },
+          { type: 'bind', command: 'camera_strafe_mouse', value: '0.03', negated: false, keys: null },
+          { type: 'bind', command: 'camera_forward_mouse', value: '0.03', negated: false, keys: null },
+          { type: 'bind', command: 'camera_forward_mouse', value: '0.03', negated: false, keys: null },
         ],
       },
 
-      //  minigame 
+      //  minigame
       {
         name: 'minigame',
         binds: [
-          { command: 'minigame_escape', value: null, negated: false, keys: null },
-          { command: 'easel_mg_boost1', value: null, negated: false, keys: null },
-          { command: 'easel_mg_boost2', value: null, negated: false, keys: null },
-          { command: 'easel_mg_boost3', value: null, negated: false, keys: null },
-          { command: 'easel_mg_heroic_boost', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'minigame_escape', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'easel_mg_boost1', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'easel_mg_boost2', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'easel_mg_boost3', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'easel_mg_heroic_boost', value: null, negated: false, keys: null },
         ],
       },
 
-      //  GLOBAL (chat & misc) 
+      //  GLOBAL (chat & misc)
       {
         name: null,
         binds: [
-          { command: 'cmd_smart_chat', value: null, negated: false, keys: null },
-          { command: 'cmd_smart_chat_cancel', value: null, negated: false, keys: null },
-          { command: 'cmd_smart_chat_1', value: null, negated: false, keys: null },
-          { command: 'cmd_smart_chat_2', value: null, negated: false, keys: null },
-          { command: 'cmd_smart_chat_3', value: null, negated: false, keys: null },
-          { command: 'cmd_smart_chat_4', value: null, negated: false, keys: null },
-          { command: 'cmd_smart_chat_5', value: null, negated: false, keys: null },
-          { command: 'cmd_smart_chat_6', value: null, negated: false, keys: null },
-          { command: 'cmd_smart_chat_7', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_smart_chat', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_smart_chat_cancel', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_smart_chat_1', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_smart_chat_2', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_smart_chat_3', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_smart_chat_4', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_smart_chat_5', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_smart_chat_6', value: null, negated: false, keys: null },
+          { type: 'bind', command: 'cmd_smart_chat_7', value: null, negated: false, keys: null },
         ],
       },
     ],
+  },
+  keybindsUiModel: {
+    talents: {
+      ...Object.fromEntries(Array.from({ length: 10 }, (_, i) => [`cmd_action_bar_slot${i + 1}`, null])),
+
+      self_cast_on: null,
+      cmd_portal: null,
+      actionbar_lock_off: null,
+    },
+    additionalTalents: {
+      ...Object.fromEntries(Array.from({ length: 14 }, (_, i) => [`cmd_action_bar_slot${i + 11}`, null]))
+    }
   },
   source: 'native' | 'browser',
   configPath: null,
